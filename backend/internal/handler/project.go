ae34faa190d14fda07d8d578134b37e6a2192649
Gabriel <gabrielvenascordeiro07@gmail.com>
2026-03-28 17:46:17 -0300

    (backend) feat: Adiciona Mutex e Verificações ao project - Utilização de Lock e Unlock em funções do projectgo. Evitando race codition. - Adiciona verificadores para campos enviados pelo frontend.

--- backend/internal/handler/project.go
+++ backend/internal/handler/project.go
@@ -5,18 +5,24 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/google/uuid"
 )
 
-var projectsDB = []models.Project{}
-
+var (
+	mu sync.Mutex
+	projectsDB = []models.Project{}
+	) 
+	
 func CreateProject(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-
+	mu.Lock()
+	defer mu.Unlock()
+		
 	userID, ok := r.Context().Value("userID").(string)
-
 	if !ok {
 		respondWithError(w, http.StatusInternalServerError, "Erro interno: usuário não identificado no contexto")
 		return
@@ -29,13 +35,30 @@ func CreateProject(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-
+	
+	if p.Name == "" {
+		respondWithError(w, http.StatusBadRequest, "O campo de nome não pode ser vazio.")
+		return
+	}
+	
+	if len(p.Name) < 3 || len(p.Name) >= 20 {
+		respondWithError(w, http.StatusBadRequest, "O nome do projeto deve ter de 4 a 20 caracteres")
+		return
+	}
+	
+	for _, v := range projectsDB {
+		if strings.EqualFold(v.Name, p.Name) {
+			respondWithError(w, http.StatusConflict, "Já existe um projeto com esse nome.")
+			return
+		}
+	}
+	
 	p.ID = uuid.New().String()
 	p.OwnerID = userID
 	p.Members = []*models.ProjectMember{}
 	p.CreatedAt = time.Now()
 	p.UpdatedAt = time.Now()
-
+	
 	projectsDB = append(projectsDB, p)
 	
 	w.WriteHeader(http.StatusCreated)
@@ -48,6 +71,8 @@ func CreateProject(w http.ResponseWriter, r *http.Request) {
 
 func ListProject(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	mu.Lock()
+	defer mu.Unlock()
 
 	w.WriteHeader(http.StatusOK)
 	err := json.NewEncoder(w).Encode(projectsDB)
@@ -58,6 +83,8 @@ func ListProject(w http.ResponseWriter, r *http.Request) {
 
 func GetProject(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	mu.Lock()
+	defer mu.Unlock()
 
 	idDesejado := r.PathValue("id")
 
@@ -76,6 +103,8 @@ func GetProject(w http.ResponseWriter, r *http.Request) {
 
 func DeleteProject(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	mu.Lock()
+	defer mu.Unlock()
 
 	idDesejado := r.PathValue("id")
 

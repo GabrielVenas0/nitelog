d7c6c040b69fd131f365f86a1573795d5be7f89a
Gabriel <gabrielvenascordeiro07@gmail.com>
2026-06-06 20:48:38 -0300

    feat: Add Explore and My Projects

--- frontend/src/api/projects.ts
+++ frontend/src/api/projects.ts
@@ -1,3 +1,4 @@
+import type { Project } from '@/types'
 import { api } from './client'
 
 export async function GetProjects(signal: AbortSignal) {
@@ -5,12 +6,26 @@ export async function GetProjects(signal: AbortSignal) {
   return response.data
 }
 
-export async function GetProjectById(project_id: string) {
+export async function GetExploreProjects(
+  signal?: AbortSignal,
+): Promise<Project[]> {
+  const response = await api.get('/explore', { signal })
+  return response.data
+}
+
+export async function GetMyProjects(signal?: AbortSignal): Promise<Project[]> {
+  const response = await api.get('/myprojects', { signal })
+  return response.data
+}
+
+export async function GetProjectById(project_id: string): Promise<Project[]> {
   const response = await api.get(`/projects/${project_id}`)
   return response.data
 }
 
-export async function CreateProjectApi(name: FormDataEntryValue | null) {
+export async function CreateProjectApi(
+  name: FormDataEntryValue | null,
+): Promise<Project[]> {
   const response = await api.post('/projects', { name: name })
   return response.data
 }
--- frontend/src/components/layout/Sidebar.tsx
+++ frontend/src/components/layout/Sidebar.tsx
@@ -1,18 +1,94 @@
+import { GetExploreProjects, GetMyProjects } from '@/api'
 import { Navlink } from '@/components/ui'
+import type { Project } from '@/types'
+import { isCancel } from 'axios'
 import { Flame, FolderOpen } from 'lucide-react'
+import { useEffect, useState } from 'react'
 
 export const Sidebar = () => {
+  const [myProjects, setMyProjects] = useState<Project[]>([])
+  const [expProjects, setExpProjects] = useState<Project[]>([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const controller = new AbortController()
+
+    async function loadMyProjects() {
+      try {
+        const data = await GetMyProjects(controller.signal)
+        setMyProjects(data)
+      } catch (error) {
+        if (isCancel(error)) return
+        console.error('[GetMyProjects] Erro: ', error)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    async function loadExploreProjects() {
+      try {
+        const data = await GetExploreProjects(controller.signal)
+        setExpProjects(data)
+      } catch (error) {
+        if (isCancel(error)) return
+        console.error('[GetMyProjects] Erro: ', error)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    loadMyProjects()
+    loadExploreProjects()
+
+    return () => controller.abort()
+  }, [])
+
   return (
-    <aside className='flex text-(--accent) h-full w-60 flex-col border-r bg-(--bg)  transition-colors'>
+    <aside className='flex h-full w-60 flex-col border-r bg-(--bg) text-(--accent) transition-colors'>
       <nav className='flex flex-col gap-2 p-2'>
         <Navlink to='/foryou'>
           <Flame className='h-5 w-5' />
           Para você
         </Navlink>
-        <Navlink to='/projects'>
-          <FolderOpen className='h-5 w-5' />
-          Projetos
-        </Navlink>
+        <div className='m-2 mt-4 flex flex-col gap-2'>
+          <div className='flex items-center gap-2'>
+            <FolderOpen className='h-5 w-5' />
+            Projetos
+          </div>
+          <div className='flex flex-col gap-1 pl-4'>
+            {isLoading}
+
+            {!isLoading && myProjects.length === 0 && (
+              <span>Nenhum projeto</span>
+            )}
+
+            {!isLoading &&
+              myProjects.map((p) => (
+                <Navlink key={p.id} to={`/projects/${p.id}`}>
+                  {p.name}
+                </Navlink>
+              ))}
+          </div>
+
+          <div className='flex items-center gap-2'>
+            <FolderOpen className='h-5 w-5' />
+            Explorar
+          </div>
+          <div className='flex flex-col gap-1 pl-4'>
+            {isLoading}
+
+            {!isLoading && expProjects.length === 0 && (
+              <span>Nenhum projeto</span>
+            )}
+
+            {!isLoading &&
+              expProjects.map((p) => (
+                <Navlink key={p.id} to={`/projects/${p.id}`}>
+                  {p.name}
+                </Navlink>
+              ))}
+          </div>
+        </div>
       </nav>
     </aside>
   )
